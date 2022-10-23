fa0a49e7de22899444cd979a3649522b4a7c2f62
Ksu <kukinaksu@yandx.ru>
2022-10-23 22:15:28 +0300

    Add the change with recommendation of teacher

--- Home_work_08/get_push_data.py
+++ Home_work_08/get_push_data.py
@@ -1,9 +1,3 @@
-from calendar import c
-from itertools import zip_longest
-from unittest import result
-from turtle import width
-
-
 def summa(a):
     result = ''
     for i in range(3):
@@ -23,21 +17,21 @@ def get_data():
     list = [summa(i) for i in zip(name, classe, adress)]
     return list
 
-def push_data(str):
-    str = str.split(';') # разделяем строку
+def push_data(strk):
+    strk = strk.split(';') # разделяем строку
     with open('name.csv', 'a', encoding='UTF-8') as name:
         for i in range(0, 5):
-            name.write(str[i] + ';')
+            name.write(strk[i] + ';')
         name.write('\n')
 
     with open('class.csv', 'a', encoding='UTF-8') as classe:
-        classe.write(str[0] + ';')
+        classe.write(strk[0] + ';')
         for i in range(5, 7):
-            classe.write(str[i] + ';')
+            classe.write(strk[i] + ';')
         classe.write('\n')
 
     with open('adress.csv', 'a', encoding='UTF-8') as adress:
-        adress.write(str[0] + ';')
+        adress.write(strk[0] + ';')
         for i in range(7, 12):
-            adress.write(str[i] + ';')
+            adress.write(strk[i] + ';')
         adress.write('\n')
